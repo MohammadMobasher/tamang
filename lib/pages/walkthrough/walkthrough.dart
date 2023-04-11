d0217015d5072c7e8117e13cbd500fa48eca0408
mohammad <m.mobasher.z@gmail.com>
2023-04-11 20:31:54 +0430

    init Walkthrough

---
+++ lib/pages/walkthrough/walkthrough.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class Walkthrough extends StatelessWidget {
+  const Walkthrough({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
