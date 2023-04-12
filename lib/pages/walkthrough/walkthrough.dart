640b5b79089a81342bff956ff8e4bc993cedb123
mohammad <m.mobasher.z@gmail.com>
2023-04-12 15:00:30 +0430

    init walkthrough page with simple pageview

--- lib/pages/walkthrough/walkthrough.dart
+++ lib/pages/walkthrough/walkthrough.dart
@@ -1,11 +1,36 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
+// ignore: must_be_immutable
 class Walkthrough extends StatelessWidget {
-  const Walkthrough({super.key});
+  Walkthrough({super.key});
+  final PageController _pageController = PageController(initialPage: 0);
+  int pageIndex = 0;
+  static const _kDuration = Duration(milliseconds: 300);
+
+  static const _kCurve = Curves.ease;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: PageView(
+              controller: _pageController,
+              clipBehavior: Clip.hardEdge,
+              scrollDirection: Axis.horizontal,
+              pageSnapping: true,
+              children: [],
+            ),
+          ),
+          SmoothPageIndicator(
+              controller: _pageController, // PageController
+              count: 4,
+              effect: const WormEffect(), // your preferred effect
+              onDotClicked: (index) {})
+        ],
+      ),
+    );
   }
 }
