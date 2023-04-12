9128d258abf04fe39b9768395143e600c6a07252
mohammad <m.mobasher.z@gmail.com>
2023-04-12 15:51:00 +0430

    complete walkthrough

--- lib/pages/walkthrough/walkthrough.dart
+++ lib/pages/walkthrough/walkthrough.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:tamang/pages/walkthrough/views/walkthrough_1/walkthrough_1.dart';
+import 'package:tamang/pages/walkthrough/views/walkthrough_2/walkthrough_2.dart';
+import 'package:tamang/pages/walkthrough/views/walkthrough_3/walkthrough_3.dart';
 
 // ignore: must_be_immutable
 class Walkthrough extends StatelessWidget {
@@ -12,24 +17,72 @@ class Walkthrough extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            child: PageView(
-              controller: _pageController,
-              clipBehavior: Clip.hardEdge,
-              scrollDirection: Axis.horizontal,
-              pageSnapping: true,
-              children: [],
-            ),
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.sp),
+          child: Column(
+            children: [
+              Flexible(
+                child: PageView(
+                  controller: _pageController,
+                  clipBehavior: Clip.hardEdge,
+                  scrollDirection: Axis.horizontal,
+                  pageSnapping: true,
+                  children: const [
+                    Walkthrough1(),
+                    Walkthrough2(),
+                    Walkthrough3(),
+                  ],
+                ),
+              ),
+              SmoothPageIndicator(
+                controller: _pageController, // PageController
+                count: 3,
+                effect: WormEffect(
+                  activeDotColor: const Color(0xFF22A45D),
+                  dotColor: const Color(0xFF979797),
+                  dotHeight: 8.h,
+                  dotWidth: 8.w,
+                ), // your preferred effect
+                onDotClicked: (index) {},
+              ),
+              SizedBox(
+                height: 10.h,
+              ),
+              SizedBox(
+                width: Get.width,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Get.toNamed(AppRoute.walkthrough);
+                  },
+                  style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.symmetric(vertical: 17.sp, horizontal: 60.sp),
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      backgroundColor: const Color(0xffEEA734),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.sp),
+                      )),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 25.sp,
+                    ),
+                    child: Text(
+                      "GET STARTED",
+                      style: TextStyle(
+                        fontSize: 17.sp,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20.h,
+              ),
+            ],
           ),
-          SmoothPageIndicator(
-              controller: _pageController, // PageController
-              count: 4,
-              effect: const WormEffect(), // your preferred effect
-              onDotClicked: (index) {})
-        ],
+        ),
       ),
     );
   }
