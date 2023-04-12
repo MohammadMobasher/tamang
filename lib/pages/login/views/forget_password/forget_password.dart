6819f90e3e1683878a6e1e056a63fefabf5eaf19
mohammad <m.mobasher.z@gmail.com>
2023-04-12 23:59:40 +0430

    add sended email in forget password with ctrl

--- lib/pages/login/views/forget_password/forget_password.dart
+++ lib/pages/login/views/forget_password/forget_password.dart
@@ -2,9 +2,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:tamang/pages/login/views/forget_password/forget_password_controller.dart';
 
 class ForgetPassword extends StatelessWidget {
-  const ForgetPassword({super.key});
+  ForgetPassword({super.key});
+  final ForgetPasswordController ctrl = Get.put(ForgetPasswordController());
 
   @override
   Widget build(BuildContext context) {
@@ -33,96 +35,154 @@ class ForgetPassword extends StatelessWidget {
         ),
         body: Padding(
           padding: EdgeInsets.symmetric(horizontal: 20.sp),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 24.h,
-              ),
-              Text(
-                "Forgot Password",
-                style: TextStyle(fontSize: 34.sp, color: Colors.black),
-              ),
-              SizedBox(
-                height: 20.h,
-              ),
-              Text(
-                "Enter your email address and we will send you a reset instructions.",
-                textAlign: TextAlign.left,
-                style: TextStyle(fontSize: 16.sp, color: const Color(0xFF868686)),
-              ),
-              SizedBox(
-                height: 34.h,
-              ),
-              TextField(
-                style: TextStyle(
-                  fontSize: 16.sp,
-                  fontWeight: FontWeight.bold,
-                ),
-                decoration: InputDecoration(
-                  isDense: true,
-                  contentPadding: EdgeInsets.symmetric(vertical: 9.sp),
-                  focusedBorder: UnderlineInputBorder(
-                    borderRadius: BorderRadius.circular(5.sp),
-                    borderSide: BorderSide(
-                      color: Colors.black,
-                      width: 0.5.sp,
+          child: Obx(() => ctrl.sendedEmail.value
+              ? Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 24.h,
                     ),
-                  ),
-                  enabledBorder: UnderlineInputBorder(
-                    borderRadius: BorderRadius.circular(5.sp),
-                    borderSide: BorderSide(
-                      color: Colors.black,
-                      width: 0.5.sp,
+                    Text(
+                      "Reset email sent",
+                      style: TextStyle(fontSize: 34.sp, color: Colors.black),
                     ),
-                  ),
-                  disabledBorder: UnderlineInputBorder(
-                    borderRadius: BorderRadius.circular(5.sp),
-                    borderSide: BorderSide(
-                      color: Colors.black,
-                      width: 0.5.sp,
+                    SizedBox(
+                      height: 20.h,
                     ),
-                  ),
-                  labelText: "EMAIL ADDRESS",
-                  labelStyle: TextStyle(
-                    fontSize: 12.sp,
-                    fontWeight: FontWeight.bold,
-                    color: const Color(0xFF868686),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 24.h,
-              ),
-              SizedBox(
-                width: Get.width,
-                child: ElevatedButton(
-                  onPressed: () {
-                    // Get.offNamed(AppRoute.walkthrough);
-                  },
-                  style: ElevatedButton.styleFrom(
-                      padding: EdgeInsets.symmetric(vertical: 17.sp, horizontal: 60.sp),
-                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                      backgroundColor: const Color(0xffEEA734),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8.sp),
-                      )),
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: 25.sp,
+                    Text(
+                      "We have sent a instructions email to sajin tamang@figma.com.",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(fontSize: 16.sp, color: const Color(0xFF868686)),
                     ),
-                    child: Text(
-                      "RESET PASSWORD",
-                      style: TextStyle(
-                        fontSize: 17.sp,
-                        fontWeight: FontWeight.bold,
+                    SizedBox(
+                      height: 24.h,
+                    ),
+                    SizedBox(
+                      width: Get.width,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          ctrl.sendedEmail.value = true;
+                        },
+                        style: ElevatedButton.styleFrom(
+                            padding: EdgeInsets.symmetric(vertical: 17.sp, horizontal: 60.sp),
+                            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                            backgroundColor: const Color(0xffEEA734),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8.sp),
+                            )),
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 25.sp,
+                          ),
+                          child: Text(
+                            "SEND AGAIN",
+                            style: TextStyle(
+                              fontSize: 17.sp,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                ),
-              )
-            ],
-          ),
+                    SizedBox(
+                      height: 123.sp,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 35.sp),
+                      child: Image.asset("assets/images/ResentEmail.png"),
+                    )
+                  ],
+                )
+              : Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 24.h,
+                    ),
+                    Text(
+                      "Forgot Password",
+                      style: TextStyle(fontSize: 34.sp, color: Colors.black),
+                    ),
+                    SizedBox(
+                      height: 20.h,
+                    ),
+                    Text(
+                      "Enter your email address and we will send you a reset instructions.",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(fontSize: 16.sp, color: const Color(0xFF868686)),
+                    ),
+                    SizedBox(
+                      height: 34.h,
+                    ),
+                    TextField(
+                      style: TextStyle(
+                        fontSize: 16.sp,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      decoration: InputDecoration(
+                        isDense: true,
+                        contentPadding: EdgeInsets.symmetric(vertical: 9.sp),
+                        focusedBorder: UnderlineInputBorder(
+                          borderRadius: BorderRadius.circular(5.sp),
+                          borderSide: BorderSide(
+                            color: Colors.black,
+                            width: 0.5.sp,
+                          ),
+                        ),
+                        enabledBorder: UnderlineInputBorder(
+                          borderRadius: BorderRadius.circular(5.sp),
+                          borderSide: BorderSide(
+                            color: Colors.black,
+                            width: 0.5.sp,
+                          ),
+                        ),
+                        disabledBorder: UnderlineInputBorder(
+                          borderRadius: BorderRadius.circular(5.sp),
+                          borderSide: BorderSide(
+                            color: Colors.black,
+                            width: 0.5.sp,
+                          ),
+                        ),
+                        labelText: "EMAIL ADDRESS",
+                        labelStyle: TextStyle(
+                          fontSize: 12.sp,
+                          fontWeight: FontWeight.bold,
+                          color: const Color(0xFF868686),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 24.h,
+                    ),
+                    SizedBox(
+                      width: Get.width,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          ctrl.sendedEmail.value = true;
+                        },
+                        style: ElevatedButton.styleFrom(
+                            padding: EdgeInsets.symmetric(vertical: 17.sp, horizontal: 60.sp),
+                            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                            backgroundColor: const Color(0xffEEA734),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8.sp),
+                            )),
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 25.sp,
+                          ),
+                          child: Text(
+                            "RESET PASSWORD",
+                            style: TextStyle(
+                              fontSize: 17.sp,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                )),
         ),
       ),
     );
