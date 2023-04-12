bc81bb0ec1f24a94494cc84530d5a7aec3a4b0d2
mohammad <m.mobasher.z@gmail.com>
2023-04-12 14:58:53 +0430

    add some padding for splash page

--- lib/pages/splash/splash.dart
+++ lib/pages/splash/splash.dart
@@ -9,50 +9,53 @@ class Splash extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Future.delayed(const Duration(seconds: 2), () async {
+    Future.delayed(const Duration(seconds: 5), () async {
       Get.offNamed(AppRoute.welcome);
     });
     return SafeArea(
       child: Scaffold(
-        body: Stack(
-          children: [
-            Image.asset(
-              "assets/images/circle_background.png",
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20.sp),
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: 95.h,
-                  ),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        "assets/images/logo.png",
-                      ),
-                      SizedBox(
-                        width: 8.w,
-                      ),
-                      const Text(
-                        "Tamang\nFoodService",
-                        style: TextStyle(fontSize: 37, fontWeight: FontWeight.bold),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 33.h,
-                  ),
-                  SvgPicture.asset(
-                    "assets/images/person_with_ice.svg",
-                    width: 214.w,
-                    height: 244.h,
-                  ),
-                ],
+        body: Padding(
+          padding: EdgeInsets.only(top: Get.height / 5),
+          child: Stack(
+            children: [
+              Image.asset(
+                "assets/images/circle_background.png",
               ),
-            ),
-          ],
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.sp),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 95.h,
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          "assets/images/logo.png",
+                        ),
+                        SizedBox(
+                          width: 8.w,
+                        ),
+                        const Text(
+                          "Tamang\nFoodService",
+                          style: TextStyle(fontSize: 37, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 33.h,
+                    ),
+                    SvgPicture.asset(
+                      "assets/images/person_with_ice.svg",
+                      width: 214.w,
+                      height: 244.h,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
