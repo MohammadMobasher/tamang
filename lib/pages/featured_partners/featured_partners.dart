c8c9bbe4dff19149f25d42d8009fc5311ce26d9b
mohammad <m.mobasher.z@gmail.com>
2023-04-14 14:55:42 +0430

    init featured_partners page

---
+++ lib/pages/featured_partners/featured_partners.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FeaturedPartners extends StatelessWidget {
+  const FeaturedPartners({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
