9fa4bae6ed5fb246988c46bfdb8b363382756628
mohammad <m.mobasher.z@gmail.com>
2023-04-17 15:56:20 +0430

    add AccountSetting items in Profile page

--- lib/pages/profile/profile.dart
+++ lib/pages/profile/profile.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tamang/widgets/m_text_main_and_details.dart';
 
 class Profile extends StatelessWidget {
   const Profile({super.key});
@@ -10,34 +12,223 @@ class Profile extends StatelessWidget {
       child: Scaffold(
         body: Padding(
           padding: EdgeInsets.symmetric(horizontal: 20.sp),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: EdgeInsets.only(left: 34.sp),
-                child: Text(
-                  "Account Settings",
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 34.sp),
+                  child: Text(
+                    "Account Settings",
+                    style: TextStyle(
+                      fontSize: 28.sp,
+                      fontWeight: FontWeight.w500,
+                      color: const Color(0xFF010F07),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20.h,
+                ),
+                Text(
+                  "Update your settings like notifications, payments, profile edit etc.",
                   style: TextStyle(
-                    fontSize: 28.sp,
-                    fontWeight: FontWeight.w500,
+                    fontSize: 16.sp,
+                    color: const Color(0xFF868686),
+                  ),
+                ),
+                SizedBox(
+                  height: 24.h,
+                ),
+                MTextMainAndDetailAndPostIcon(
+                  preIcon: Icon(
+                    CupertinoIcons.person_fill,
+                    size: 24.sp,
+                    color: const Color(0xFF010F07),
+                  ),
+                  isCenter: true,
+                  mainDetailPadding: 12.sp,
+                  maintext: Text(
+                    "Profile Information",
+                    style: TextStyle(
+                      fontSize: 16.sp,
+                      color: const Color(0xFF010F07),
+                    ),
+                  ),
+                  detailsText: Text(
+                    "Change your account information",
+                    style: TextStyle(
+                      fontSize: 14.sp,
+                      color: const Color(0xFF868686),
+                    ),
+                  ),
+                  postWidget: Icon(
+                    CupertinoIcons.forward,
+                    size: 24.sp,
                     color: const Color(0xFF010F07),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 20.h,
-              ),
-              Text(
-                "Update your settings like notifications, payments, profile edit etc.",
-                style: TextStyle(
-                  fontSize: 16.sp,
-                  color: const Color(0xFF868686),
+                const Divider(
+                  color: Color(0xFF979797),
+                  thickness: 0.2,
                 ),
-              ),
-              SizedBox(
-                height: 24.h,
-              ),
-            ],
+                MTextMainAndDetailAndPostIcon(
+                  preIcon: Icon(
+                    CupertinoIcons.lock_fill,
+                    size: 24.sp,
+                    color: const Color(0xFF010F07),
+                  ),
+                  isCenter: true,
+                  maintext: Text(
+                    "Change Password",
+                    style: TextStyle(
+                      fontSize: 16.sp,
+                      color: const Color(0xFF010F07),
+                    ),
+                  ),
+                  detailsText: Text(
+                    "Change your password",
+                    style: TextStyle(
+                      fontSize: 14.sp,
+                      color: const Color(0xFF868686),
+                    ),
+                  ),
+                  postWidget: Icon(
+                    CupertinoIcons.forward,
+                    size: 24.sp,
+                    color: const Color(0xFF010F07),
+                  ),
+                ),
+                const Divider(
+                  color: Color(0xFF979797),
+                  thickness: 0.2,
+                ),
+                MTextMainAndDetailAndPostIcon(
+                  preIcon: Icon(
+                    CupertinoIcons.creditcard,
+                    size: 24.sp,
+                    color: const Color(0xFF010F07),
+                  ),
+                  isCenter: true,
+                  maintext: Text(
+                    "Payment Methods",
+                    style: TextStyle(
+                      fontSize: 16.sp,
+                      color: const Color(0xFF010F07),
+                    ),
+                  ),
+                  detailsText: Text(
+                    "Add your credit & debit cards",
+                    style: TextStyle(
+                      fontSize: 14.sp,
+                      color: const Color(0xFF868686),
+                    ),
+                  ),
+                  postWidget: Icon(
+                    CupertinoIcons.forward,
+                    size: 24.sp,
+                    color: const Color(0xFF010F07),
+                  ),
+                ),
+                const Divider(
+                  color: Color(0xFF979797),
+                  thickness: 0.2,
+                ),
+                MTextMainAndDetailAndPostIcon(
+                  preIcon: Icon(
+                    CupertinoIcons.location_solid,
+                    size: 24.sp,
+                    color: const Color(0xFF010F07),
+                  ),
+                  isCenter: true,
+                  maintext: Text(
+                    "Locations",
+                    style: TextStyle(
+                      fontSize: 16.sp,
+                      color: const Color(0xFF010F07),
+                    ),
+                  ),
+                  detailsText: Text(
+                    "Add or remove your delivery locations",
+                    style: TextStyle(
+                      fontSize: 14.sp,
+                      color: const Color(0xFF868686),
+                    ),
+                  ),
+                  postWidget: Icon(
+                    CupertinoIcons.forward,
+                    size: 24.sp,
+                    color: const Color(0xFF010F07),
+                  ),
+                ),
+                const Divider(
+                  color: Color(0xFF979797),
+                  thickness: 0.2,
+                ),
+                MTextMainAndDetailAndPostIcon(
+                  preIcon: Icon(
+                    CupertinoIcons.location_solid,
+                    size: 24.sp,
+                    color: const Color(0xFF010F07),
+                  ),
+                  isCenter: true,
+                  maintext: Text(
+                    "Add Social Account",
+                    style: TextStyle(
+                      fontSize: 16.sp,
+                      color: const Color(0xFF010F07),
+                    ),
+                  ),
+                  detailsText: Text(
+                    "Add Facebook, Twitter etc",
+                    style: TextStyle(
+                      fontSize: 14.sp,
+                      color: const Color(0xFF868686),
+                    ),
+                  ),
+                  postWidget: Icon(
+                    CupertinoIcons.forward,
+                    size: 24.sp,
+                    color: const Color(0xFF010F07),
+                  ),
+                ),
+                const Divider(
+                  color: Color(0xFF979797),
+                  thickness: 0.2,
+                ),
+                MTextMainAndDetailAndPostIcon(
+                  preIcon: Icon(
+                    CupertinoIcons.arrow_uturn_right_circle_fill,
+                    size: 24.sp,
+                    color: const Color(0xFF010F07),
+                  ),
+                  isCenter: true,
+                  maintext: Text(
+                    "Refer to Friends",
+                    style: TextStyle(
+                      fontSize: 16.sp,
+                      color: const Color(0xFF010F07),
+                    ),
+                  ),
+                  detailsText: Text(
+                    "Get \$10 for reffering friends",
+                    style: TextStyle(
+                      fontSize: 14.sp,
+                      color: const Color(0xFF868686),
+                    ),
+                  ),
+                  postWidget: Icon(
+                    CupertinoIcons.forward,
+                    size: 24.sp,
+                    color: const Color(0xFF010F07),
+                  ),
+                ),
+                SizedBox(
+                  height: 28.h,
+                ),
+              ],
+            ),
           ),
         ),
       ),
