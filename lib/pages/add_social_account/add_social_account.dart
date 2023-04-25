1c8867b96bd05e80073d657ab4663fd09a37f6a8
mohammad <m.mobasher.z@gmail.com>
2023-04-25 16:03:59 +0430

    add appbar to add_sicial_account page

--- lib/pages/add_social_account/add_social_account.dart
+++ lib/pages/add_social_account/add_social_account.dart
@@ -1,10 +1,38 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 
 class AddSocialAccount extends StatelessWidget {
   const AddSocialAccount({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+        child: Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        titleSpacing: 0,
+        centerTitle: true,
+        leading: InkWell(
+          onTap: () {
+            Get.back();
+          },
+          child: Icon(
+            CupertinoIcons.back,
+            size: 24.sp,
+          ),
+        ),
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        title: Text(
+          "Add Social Account",
+          style: TextStyle(
+            fontSize: 16.sp,
+            color: const Color(0xFF010F07),
+          ),
+        ),
+      ),
+    ));
   }
 }
